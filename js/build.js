803ac060b5dc43f6df291009078320fc71815ac4
Jake Greer <jakegreer11@gmail.com>
2019-09-18 23:08:36 -0700

    updated styles

--- js/build.js
+++ js/build.js
@@ -19,19 +19,19 @@ class Model {
             {
                 hasHeader: false,
                 linkText: 'Fit Guide',
-                bodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nibh arcu, ultricies nec purus quis, consequat luctus orci. Sed non mi nisi. Donec vulputate erat odio, eget lacinia lectus accumsan sed. Phasellus diam lorem, ullamcorper quis velit a, ultricies facilisis turpis. Sed laoreet efficitur odio, ut consequat arcu iaculis non. Quisque lectus ligula, venenatis quis ullamcorper vitae, euismod in nisi. Sed sed arcu tortor. Phasellus a iaculis metus, sed suscipit dui. Nunc mollis, ipsum at tristique dignissim, enim mi sodales nulla, sed dapibus lorem tortor et nisi.',
+                bodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat est velit egestas dui id ornare arcu odio. Integer quis auctor elit sed vulputate. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros in. Sapien pellentesque habitant morbi tristique senectus et netus et malesuada. Mi proin sed libero enim sed faucibus. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum. Venenatis urna cursus eget nunc scelerisque viverra mauris in. Nibh tortor id aliquet lectus proin nibh. Euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Lectus nulla at volutpat diam ut venenatis tellus in. Bibendum est ultricies integer quis auctor elit sed vulputate. Odio morbi quis commodo odio aenean.',
                 active: true
             },
             {
                 hasHeader: false,
                 linkText: 'Care',
-                bodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nibh arcu, ultricies nec purus quis, consequat luctus orci. Sed non mi nisi. Donec vulputate erat odio, eget lacinia lectus accumsan sed. Phasellus diam lorem, ullamcorper quis velit a, ultricies facilisis turpis. Sed laoreet efficitur odio, ut consequat arcu iaculis non. Quisque lectus ligula, venenatis quis ullamcorper vitae, euismod in nisi. Sed sed arcu tortor. Phasellus a iaculis metus, sed suscipit dui. Nunc mollis, ipsum at tristique dignissim, enim mi sodales nulla, sed dapibus lorem tortor et nisi.',
+                bodyText: 'Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices tincidunt arcu non sodales neque sodales ut. Ipsum dolor sit amet consectetur adipiscing. Faucibus vitae aliquet nec ullamcorper. Ac turpis egestas maecenas pharetra. Pellentesque id nibh tortor id. Semper eget duis at tellus at. Elementum nibh tellus molestie nunc non blandit massa. Facilisis mauris sit amet massa vitae tortor condimentum. Justo nec ultrices dui sapien eget mi proin sed. Eu scelerisque felis imperdiet proin. In hendrerit gravida rutrum quisque non tellus orci. Aliquet enim tortor at auctor urna. Tristique senectus et netus et malesuada fames ac. Enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra. Nunc scelerisque viverra mauris in aliquam sem. Purus sit amet luctus venenatis lectus. Vitae proin sagittis nisl rhoncus.',
                 active: false
             },
             {
                 hasHeader: true,
                 linkText: 'Materials',
-                bodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nibh arcu, ultricies nec purus quis, consequat luctus orci. Sed non mi nisi. Donec vulputate erat odio, eget lacinia lectus accumsan sed. Phasellus diam lorem, ullamcorper quis velit a, ultricies facilisis turpis. Sed laoreet efficitur odio, ut consequat arcu iaculis non. Quisque lectus ligula, venenatis quis ullamcorper vitae, euismod in nisi. Sed sed arcu tortor. Phasellus a iaculis metus, sed suscipit dui. Nunc mollis, ipsum at tristique dignissim, enim mi sodales nulla, sed dapibus lorem tortor et nisi.',
+                bodyText: 'Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nibh arcu, ultricies nec purus quis, consequat luctus orci. Sed non mi nisi. Donec vulputate erat odio, eget lacinia lectus accumsan sed. Phasellus diam lorem, ullamcorper quis velit a, ultricies facilisis turpis. Sed laoreet efficitur odio, ut consequat arcu iaculis non. Quisque lectus ligula, venenatis quis ullamcorper vitae, euismod in nisi. Sed sed arcu tortor. Phasellus a iaculis metus, sed suscipit dui. Nunc mollis, ipsum at tristique dignissim, enim mi sodales nulla, sed dapibus lorem tortor et nisi.',
                 active: false
             }
         ]
@@ -83,40 +83,42 @@ class View {
                 // Create nodes
                 var tabNumber = i + 1;
                 var li = this.createElement('li', 'tab-title');
-                var link = this.createElement('a');
+                var link = this.createElement('a', 'tab');
+                var accordionContainer = this.createElement('div', 'accordion-tab');
+                var accordionHeader = this.createElement('h3');
+                var accordionIcon = this.createElement('span')
                 var panel = this.createElement('div', 'tab-panel');
                 var panelBody = this.createElement('p', 'tab-body');
 
                 // Set Attributes and Text
                 link.setAttribute('data-index', i);
+                accordionContainer.setAttribute('data-index', i);
                 link.href = "#panel" + tabNumber;
                 link.textContent = tab.linkText;
+                accordionHeader.textContent = tab.linkText;
+                accordionIcon.innerHTML = "&#x2b;";
                 panel.setAttribute('id', 'panel#' + tabNumber);
                 panelBody.textContent = tab.bodyText;
 
                 // Add event listener
-                link.addEventListener('click', event => {
-                    var index = event.target.dataset.index;
-                    if(index) {
-                        handler(index);
-                    }
-                    else {
-                        //Debugging
-                        console.log("Error tab number can't contain: ", index);
-                    }
-                })
+                var handleClick = event => this._handleTabClick(event, handler)
+                link.addEventListener('click', handleClick );
+                accordionContainer.addEventListener('click', handleClick);
 
                 //Set classes for active tab
                 if(tab.active) {
                     li.classList.add('active');
                     panel.classList.add('active');
+                    accordionContainer.classList.add('active');
+                    accordionIcon.innerHTML = '&#8722;';
                 }
 
                 // Append nodes
                 li.append( link );
                 panel.append(panelBody);
+                accordionContainer.append( accordionHeader, accordionIcon);
                 this.tabList.append(li);
-                this.tabContent.append(panel);
+                this.tabContent.append(accordionContainer, panel);
                 
             })
         }
@@ -124,11 +126,26 @@ class View {
         // Debugging
         console.log(tabs)
     }
+
+    _handleTabClick = (event, handler) => {
+        var index = event.currentTarget.dataset.index;
+        if(index) {
+            handler(index);
+        }
+        else {
+            //Debugging
+            console.log("Error tab number can't contain: ", index);
+        }
+    }
   
     createElement(tag, className) {
         const element = document.createElement(tag)
-    
-        if (className) element.classList.add(className)
+        if (className) {
+            var classes = className.split(' ');
+            for(var i = 0; i < classes.length; i++) {
+                element.classList.add(classes[i])
+            }
+        }
     
         return element
     }
@@ -141,18 +158,22 @@ class View {
 
     displayTabs(tabs) {
 
-        var length = tabs.length;
+        var length = tabs.length; //Will contain the same length as all HTML Collections below.
+
         var tabLinks = this.tabList.children;
-        var tabPanels = this.tabContent.children;
+        var tabPanels = this.tabContent.querySelectorAll('.tab-panel');
+        var accordionIcons = this.tabContent.querySelectorAll('.accordion-tab span');
 
         for(var i = 0; i < length; i++) {
             if(tabs[i].active) {
                 tabLinks[i].classList.add('active');
                 tabPanels[i].classList.add('active');
+                accordionIcons[i].innerHTML = '&#8722;'
             }
             else {
                 tabLinks[i].classList.remove('active');
                 tabPanels[i].classList.remove('active');
+                accordionIcons[i].innerHTML = '&#x2b;';
             }
         }
     }

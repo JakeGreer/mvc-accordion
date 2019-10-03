841157a3c84cb3c486afa6a8c4337bd6f26b52b4
Jake Greer <jakegreer11@gmail.com>
2019-10-02 22:08:30 -0700

    added custom animated icons

--- js/build.js
+++ js/build.js
@@ -1,7 +1,7 @@
 /**
  * Name: Jake Greer
- * Email: jakegreer11@gmail.com
- * Date: 9/18/2019
+ * Email: jakegreer93@gmail.com
+ * Date: 9/19/2019
  * Project: Responsive Tabs
  * Description: Browser compatible, responsive, pure javascript, user interface tabs component that converts to an accordion in mobile.
  */
@@ -28,7 +28,7 @@ class Model {
             },
             {
                 linkText: 'Materials',
-                customContent: '<ul><li><h2>50%</h2><span>cashmere</span></li><li><h2>46%</h2><span>wool</span></li><li><h2>4%</h2><span>modal</span></li></ul>',
+                customContent: '<div class="row"><div class="column"><h2>50%</h2><span>cashmere</span></div><div class="column"><h2>46%</h2><span>wool</span></div><div class="column"><h2>4%</h2><span>modal</span></div></div>',
                 bodyText: 'Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nibh arcu, ultricies nec purus quis, consequat luctus orci. Sed non mi nisi. Donec vulputate erat odio, eget lacinia lectus accumsan sed. Phasellus diam lorem, ullamcorper quis velit a, ultricies facilisis turpis. Sed laoreet efficitur odio, ut consequat arcu iaculis non. Quisque lectus ligula, venenatis quis ullamcorper vitae, euismod in nisi. Sed sed arcu tortor. Phasellus a iaculis metus, sed suscipit dui. Nunc mollis, ipsum at tristique dignissim, enim mi sodales nulla, sed dapibus lorem tortor et nisi.',
                 active: false
             }
@@ -84,7 +84,9 @@ class View {
                 var link = this.createElement('a', 'tab');
                 var accordionContainer = this.createElement('div', 'accordion-tab');
                 var accordionHeader = this.createElement('h3');
-                var accordionIcon = this.createElement('span')
+                var accordionIcon = this.createElement('span');
+                var iconHorizontal = this.createElement('div', 'horizontal');
+                var iconVertical = this.createElement('div', 'vertical');
                 var panel = this.createElement('div', 'tab-panel');
                 var panelBody = this.createElement('p', 'tab-body');
                 var customContent = null;
@@ -98,7 +100,7 @@ class View {
                 link.href = "#panel" + tabNumber;
                 link.textContent = tab.linkText;
                 accordionHeader.textContent = tab.linkText;
-                accordionIcon.innerHTML = "&#x2b;";
+                accordionIcon.classList.add("icon-plus");
                 panel.setAttribute('id', 'panel#' + tabNumber);
                 panelBody.textContent = tab.bodyText;
 
@@ -112,13 +114,14 @@ class View {
                     li.classList.add('active');
                     panel.classList.add('active');
                     accordionContainer.classList.add('active');
-                    accordionIcon.innerHTML = '&#8722;';
+                    accordionIcon.classList.add("icon-minus");
                 }
 
                 // Append nodes
                 li.append( link );
                 if(customContent) panel.append(customContent);
                 panel.append(panelBody);
+                accordionIcon.append(iconHorizontal, iconVertical);
                 accordionContainer.append( accordionHeader, accordionIcon);
                 this.tabList.append(li);
                 this.tabContent.append(accordionContainer, panel);
@@ -161,18 +164,21 @@ class View {
 
         var tabLinks = this.tabList.children;
         var tabPanels = this.tabContent.querySelectorAll('.tab-panel');
+        var accordionTabs = this.tabContent.querySelectorAll('.accordion-tab');
         var accordionIcons = this.tabContent.querySelectorAll('.accordion-tab span');
 
         for(var i = 0; i < length; i++) {
             if(tabs[i].active) {
                 tabLinks[i].classList.add('active');
                 tabPanels[i].classList.add('active');
-                accordionIcons[i].innerHTML = '&#8722;'
+                accordionTabs[i].classList.add('active');
+                accordionIcons[i].classList.add("icon-minus");
             }
             else {
                 tabLinks[i].classList.remove('active');
                 tabPanels[i].classList.remove('active');
-                accordionIcons[i].innerHTML = '&#x2b;';
+                accordionTabs[i].classList.remove('active');
+                accordionIcons[i].classList.remove("icon-minus");
             }
         }
     }
